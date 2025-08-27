528a820f1a6bdc6e506a3fe69daf3ceca003b1e8
Design Dile <thedesigndile@gmail.com>
2025-08-27 07:44:50 +0000

    i enter this code to terminal npm install firebase here is my sdk, i don

---
+++ src/lib/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBDg7XkkjiHGBo9fh1Y6JtwrEKxp-u2QcU",
+  authDomain: "offline-toolkit-4jn7l.firebaseapp.com",
+  projectId: "offline-toolkit-4jn7l",
+  storageBucket: "offline-toolkit-4jn7l.firebasestorage.app",
+  messagingSenderId: "919241860042",
+  appId: "1:919241860042:web:40143bda67450d1e5c6533"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
