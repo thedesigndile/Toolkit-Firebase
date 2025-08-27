dfd20f1519555678d018fb0373c65a611b5002ab
Design Dile <thedesigndile@gmail.com>
2025-08-27 19:35:44 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -53,7 +53,7 @@ export function Header() {
                     onMouseEnter={() => setHoveredItem(item.name)}
                  >
                     {item.href ? (
-                         <Link href={item.href} legacyBehavior passHref>
+                         <Link href={item.href} passHref>
                             <NavigationMenuLink className={cn(navigationMenuTriggerStyle(), "font-semibold text-sm bg-transparent text-white hover:text-white/90 px-4 py-2 hover:bg-transparent focus:bg-transparent")}>
                                 {item.name}
                             </NavigationMenuLink>
